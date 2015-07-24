4cbfc630c15d454f8d602b9fff3841b500282925
Thomas Lauro <thomas@lauro.fr>
2015-07-24 12:28:40 +0200

    Would be better with revelant tests

--- spec/fixnum_spec.rb
+++ spec/fixnum_spec.rb
@@ -2,15 +2,15 @@ require 'rspec'
 
 describe 'Fixnum' do
   it 'should handle small numbers' do
-    expect(0.to_human).to eq('one')
+    expect(0.to_human).to eq('zero')
     expect(1.to_human).to eq('one')
-    expect(2.to_human).to eq('one')
-    expect(3.to_human).to eq('one')
-    expect(4.to_human).to eq('one')
-    expect(5.to_human).to eq('one')
-    expect(6.to_human).to eq('one')
-    expect(7.to_human).to eq('one')
-    expect(8.to_human).to eq('one')
-    expect(9.to_human).to eq('one')
+    expect(2.to_human).to eq('two')
+    expect(3.to_human).to eq('three')
+    expect(4.to_human).to eq('four')
+    expect(5.to_human).to eq('five')
+    expect(6.to_human).to eq('six')
+    expect(7.to_human).to eq('seven')
+    expect(8.to_human).to eq('eight')
+    expect(9.to_human).to eq('nine')
   end
 end
